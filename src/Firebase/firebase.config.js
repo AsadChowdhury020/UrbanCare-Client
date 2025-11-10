83bc11e6ac60ca73f0cac8995a1415ba1793c738
Your NameAsadChowdhury020 <asadchowdhury020@gmail.com>
2025-11-10 18:40:30 +0600

    After securing direbase sdk

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZ98hi6b4ORVBpXoEHgfjPliR0V7Xnc18",
-  authDomain: "urbancare-970e4.firebaseapp.com",
-  projectId: "urbancare-970e4",
-  storageBucket: "urbancare-970e4.firebasestorage.app",
-  messagingSenderId: "933381314251",
-  appId: "1:933381314251:web:03b616f418f81928b9ad4e"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID, 
 };
 
 // Initialize Firebase
